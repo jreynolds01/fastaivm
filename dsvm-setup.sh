177cc6843e7bc0de2375a63e5de41f5f7ad37b53
Jeremy Reynolds <jeremy.reynolds@microsoft.com>
2018-08-24 18:54:30 -0600

    update to try to deal with a unicode error

--- dsvm-setup.sh
+++ dsvm-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This should set up an Ubuntu DSVM for this course
-# Assumes you've deployed an NC-series VM, so CUDA
+# Assumes you have deployed an NC-series VM, so CUDA
 # is already installed in the py35 conda env
 # this is based on the last part of 
 # http://files.fast.ai/setup/paperspace
@@ -11,26 +11,10 @@
 #    jupyter sees it by default
 # 2. makes sure the data is downloaded in the ~/notebooks/data directory, and linked inside fastai/courses/dl1
 
-## Usage approach number 1:
-## You can use this files via direct download:
-## wget https://raw.githubusercontent.com/jreynolds01/fastai/dsvm-setup/dsvm-setup.sh
-## chmod u+x dsvm-setup.sh
-## ./dsvm-setup.sh
-
-## Usage approach number 2:
-## Clone and run from github using git
-## logged into the DSVM:
-## cd ~/notebooks
-## ## just using this repo/branch because this script isn't official.
-## git clone https://github.com/jreynolds01/fastai.git
-## cd fastai
-## git checkout dsvm-setup
-## ./dsvm-setup.sh
-
 WD=~/notebooks
 
 cd $WD
-echo "Working in `pwd`"
+echo "Working in $(pwd)"
 
 if [ -d "fastai" ]; then
     echo "fastai already exists."
@@ -67,7 +51,6 @@ ln -s ~/data ./
 
 # not sure utility of this - will come back after walking through course...
 ## just use the conda env in the repository
-## need to clean up to make sure there's space...
 conda clean -a
 ## now create the env...
 conda env create -f ${WD}/fastai/environment.yml
@@ -80,8 +63,6 @@ sudo /anaconda/envs/fastai/bin/python -m ipykernel install --name fastai
 echo "Done!"
 
 #jupyter notebook --generate-config
-#echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
-#echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
 
 # already installed==7.2.1 (should check version requirements)
 #pip install ipywidgets
