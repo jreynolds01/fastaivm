8c418c653b3c41977bb33ab5eebde237feffee3f
Jeremy Reynolds <jeremy.reynolds@microsoft.com>
2018-08-25 09:29:45 -0600

    update typo in chown

--- dsvm-setup.sh
+++ dsvm-setup.sh
@@ -69,8 +69,8 @@ sudo /anaconda/envs/fastai/bin/python -m ipykernel install --name fastai
 # source activate py35
 
 ## update appropriate permissions
-chown -R ${$adminUser}:${$adminUser} ${WD}/data 
-chown -R ${$adminUser}:${$adminUser} ${WD}/fastai
+chown -R ${adminUser}:${adminUser} ${WD}/data 
+chown -R ${adminUser}:${adminUser} ${WD}/fastai
 
 echo "Done!"
 
